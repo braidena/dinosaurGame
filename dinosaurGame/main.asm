fb197ba0b8d1499147d7ad138aada1c327424195
braidena <bames2024@my.fit.edu>
2025-09-26 17:30:29 -0400

    Created a box and cactus, not centered though

--- dinosaurGame/main.asm
+++ dinosaurGame/main.asm
@@ -1,10 +1,56 @@
 INCLUDE Irvine32.inc
 .data
-A SDWORD 6
-B SDWORD 7
-N SDWORD 9
+boxTB BYTE "===========================================================",13,10,0
+boxLR BYTE "|                                                         |",13,10,0
+cactusTest BYTE "\|/",13,10,0
+cactusTest2 BYTE "|||",13,10,0
 .code
+createBox proc
+mov edi,OFFSET boxTB
+mov esi,OFFSET boxLR
+
+mov dl, 39
+mov dh, 11
+call Gotoxy
+
+mov edx,edi
+call WriteString
+
+mov ecx,8
+mov al,12
+
+
+printSides:
+	mov dl, 39
+	mov dh, al
+	inc al
+	call Gotoxy
+	mov edx,esi
+	call WriteString
+	loop printSides
+
+
+mov dl, 39
+mov dh, al
+call Gotoxy
+mov edx,edi
+call WriteString
+
+mov dl, 44
+mov dh, 18
+call Gotoxy
+mov edx, OFFSET cactusTest
+call WriteString
+mov dl, 44
+mov dh, 19
+call Gotoxy
+mov edx, OFFSET cactusTest2
+call WriteString
+ret
+createBox endp
+
 main PROC
-  exit
-main ENDP
+ call createBox
+ exit
+ main ENDP
 END main
