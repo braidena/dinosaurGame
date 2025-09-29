2feaf29ed069b289abafa0fa34ef959cc4af46f5
braidena <bames2024@my.fit.edu>
2025-09-29 18:08:26 -0400

    Added a moving top part of cactus, for use as the base to move everything else

--- dinosaurGame/main.asm
+++ dinosaurGame/main.asm
@@ -4,7 +4,10 @@ boxTB BYTE "===========================================================",13,10,0
 boxLR BYTE "|                                                         |",13,10,0
 cactusTest BYTE "\|/",13,10,0
 cactusTest2 BYTE "|||",13,10,0
-gameLoop BYTE 0
+gameLoopBit BYTE 0
+cactusXPos BYTE 5
+cactusYPos BYTE 5
+space BYTE "              ",0
 .code
 createBox proc
 mov edi,OFFSET boxTB
@@ -50,28 +53,56 @@ call WriteString
 ret
 createBox endp
 
-loopTest proc
+game proc
 mov ecx, 0
-textLoop:
-	.IF	gameLoop == 0
+gameLoop:
+	.IF	gameLoopBit == 0 ; while true
+	mov dl, [cactusXPos]
+	mov dh, [cactusYPos]
+	call Gotoxy 
+	inc [cactusXPos]
+	mov dl, [cactusXPos]
+	cmp dl, 11
+	je resetCactus
+
+printCactus:
+	mov dl, [cactusXPos-1]
+	mov dh, [cactusYPos]
+	call Gotoxy
+	mov edx,OFFSET space
+	call WriteString
+	mov dl, [cactusXPos]
+	mov dh, [cactusYPos]
+	call Gotoxy
 	mov edx, OFFSET cactusTest
 	call WriteString
-	inc ecx
-	cmp ecx,2
-	je changeGameLoop
-	jmp textLoop
+
+	
+
+
+	inc ecx ; just to make it not infinite for now
+	cmp ecx,50
+	je exitGameLoop
+	mov eax, 100 ; 0.1 seconds
+	call Delay
+	jmp gameLoop
+
 	.ELSE
 	ret
 	.ENDIF
-	changeGameLoop:
-		mov edi, offset gameLoop
+	resetCactus:
+		mov [cactusXPos],5
+		jmp printCactus
+
+	exitGameLoop: ; game loop false, aka = 1
+		mov edi, offset gameLoopBit
 		mov DWORD PTR [edi],1
-		jmp textLoop
-loopTest endp
+		jmp gameLoop
+game endp
 
 main PROC
  call createBox
- call loopTest
+ call game
  exit
  main ENDP
 END main
